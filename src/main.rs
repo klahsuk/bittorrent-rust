64a281ad73cc903741872fd27044f069199bac5a
kushal karamchandani <kushaldk19@gmail.com>
2026-04-10 13:47:59 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,64 @@
-use std::env;
+use std::{env};
+
+use serde_json::Value;
 
 // Available if you need it!
 // use serde_bencode
 
 #[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
+fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str){
         // Example: "5:hello" -> "hello"
-        if encoded_value.chars().nth(0) == Some('i'){
-            if let Some(end) = encoded_value.find('e'){
-                return serde_json::Value::Number(encoded_value[1..end].parse().unwrap())
-            } else {
-                panic!("Unhandled encoded value: {}", encoded_value)
-            }
+       if let Some (start) = encoded_value.chars().next(){
+        match start {
+            'i' => parse_int(encoded_value),
+            'l' => {
+                parse_list(encoded_value)
+            },
+            '0'..='9' => parse_string(encoded_value),
+            _ => panic!("Unhandled encoded value: {}", encoded_value)
         }
-        if let Some((len, rest)) = encoded_value.split_once(":"){
-            if let Ok(len) = len.parse::<usize>(){
-                return serde_json::Value::String(rest[..len].to_string());
-            }
-            else {
-                panic!("Unhandled encoded value: {}", encoded_value)
-            }
-        } else {
-            panic!("Unhandled encoded value: {}", encoded_value)
-        }   
+       } else {
+        panic!("Unhandled encoded value: {}", encoded_value)
     }
+}
+
+fn parse_int(encoded_value: &str) -> (serde_json::Value, &str) {
+     if let Some(end) = encoded_value.find('e'){
+        let val = serde_json::Value::Number(encoded_value[1..end].parse().unwrap());
+        return (val , &encoded_value[end + 1 ..])
+    } else {
+        panic!("Unhandled encoded value: {}", encoded_value)
+    }
+}
+
+fn parse_string(encoded_value: &str) -> (serde_json::Value, &str) {
+    if let Some((len, rest)) = encoded_value.split_once(":"){
+        if let Ok(len) = len.parse::<usize>(){
+            let val = serde_json::Value::String(rest[..len].to_string());
+            return (val , &encoded_value[len + 2 ..])
+        }
+        else {
+            panic!("Unhandled encoded value: {}:{}",len, rest )
+        }
+    } else {
+        panic!("Unhandled encoded value: {}", encoded_value)
+    }   
+}
+
+fn parse_list(encoded_value: &str) -> (Value, &str){
+    let mut lst = Vec::new();
+    let mut rest = encoded_value.split_at(1).1;
+        println!("received {:?}", rest);
+
+    while !rest.is_empty() && !rest.starts_with('e'){
+        let (val, remainder) = decode_bencoded_value(rest);
+        println!("remainder {:?}", remainder);
+
+        lst.push(val);
+        rest = remainder;
+    }
+    return (Value::Array(lst), &rest)
+}
 
 // Usage: your_program.sh decode "<encoded_value>"
 fn main() {
@@ -38,7 +72,7 @@ fn main() {
         // TODO: Uncomment the code below to pass the first stage
         let encoded_value = &args[2];
         let decoded_value = decode_bencoded_value(encoded_value);
-        println!("{}", decoded_value.to_string());
+        println!("{}", decoded_value.0.to_string());
     } else {
         println!("unknown command: {}", args[1])
     }
